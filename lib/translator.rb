fb81be37f8367a18925a8acf290d657f245f5e2d
Max Sun <max.sun@yale.edu>
2019-05-21 06:20:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,9 +3,8 @@ require "yaml"
 require "pry"
 def load_library(file_path)
   translations = YAML::load_file("#{file_path}")
-  puts translations
   translations.each do |meaning, array|
-
+    
   end
 end
 
